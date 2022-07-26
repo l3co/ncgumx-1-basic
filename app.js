af49ff700582e4dde713f808aa7d223a7b6fe13a
leandro costa <lcosta.sp.br@gmail.com>
2022-07-26 10:37:59 -0300

    feat(section-1): :sparkles: create routes to work with message

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+function index(_, res) {
+    res.write(`
+            <html>
+                <head>
+                    <title>Node Js Course</title>
+                </head>
+                <body>
+                    <h1>My first server with html</h1>
+                </body>
+            </html>
+        `)
+    return res.end();
+}
 
-    if (req.url === '/message') {
-        res.write(`
+function message(_, res) {
+    res.write(`
             <html>
                 <head>
                     <title>Node Js Course</title>
@@ -16,20 +28,39 @@ const server = http.createServer((req, res) => {
                 </body>
             </html>
         `)
-        return res.end();
+    return res.end();
+}
+
+function writeMessage(req, res) {
+
+}
+
+function defaultWebPage(req, res) {
+    res.write(`
+    <html>
+        <head><title>Construction</title></head>
+        <body>
+            <h1>Sorry but we don't implement the page requested ${req.url} </h1>
+        </body>
+    </html>
+    `)
+    return res.end();
+}
+
+const server = http.createServer((req, res) => {
+
+    const method = req.method;
+    console.log(method)
+
+    switch (req.url) {
+        case "/":
+            return index(req, res);
+        case "/message":
+            return message(req, res) ? method === 'GET' : writeMessage(req, res)
+        default:
+            return defaultWebPage(req, res);
     }
 
-    res.write(`
-            <html>
-                <head>
-                    <title>Node Js Course</title>
-                </head>
-                <body>
-                    <h1>My first server with html</h1>
-                </body>
-            </html>
-        `)
-    return res.end();
 })
 
 server.listen('3000', () => {
