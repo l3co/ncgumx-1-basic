d0cec9d402faf66b1ad2a430f09d282c301e5703
leandro costa <lcosta.sp.br@gmail.com>
2022-07-27 12:15:53 -0300

    feat(section-1): :art: split my app file in controllers and routes

--- app.js
+++ app.js
@@ -1,67 +1,8 @@
 const http = require('http');
+const routes = require('./routes')
 
-function index(_, res) {
-    res.write(`
-            <html>
-                <head>
-                    <title>Node Js Course</title>
-                </head>
-                <body>
-                    <h1>My first server with html</h1>
-                </body>
-            </html>
-        `)
-    return res.end();
-}
 
-function message(_, res) {
-    res.write(`
-            <html>
-                <head>
-                    <title>Node Js Course</title>
-                </head>
-                <body>
-                    <form action="/message" method="POST">
-                        <input type="text" name="message">
-                        <input type="submit" value="Send" />
-                    </form>
-                </body>
-            </html>
-        `)
-    return res.end();
-}
-
-function writeMessage(req, res) {
-
-}
-
-function defaultWebPage(req, res) {
-    res.write(`
-    <html>
-        <head><title>Construction</title></head>
-        <body>
-            <h1>Sorry but we don't implement the page requested ${req.url} </h1>
-        </body>
-    </html>
-    `)
-    return res.end();
-}
-
-const server = http.createServer((req, res) => {
-
-    const method = req.method;
-    console.log(method)
-
-    switch (req.url) {
-        case "/":
-            return index(req, res);
-        case "/message":
-            return message(req, res) ? method === 'GET' : writeMessage(req, res)
-        default:
-            return defaultWebPage(req, res);
-    }
-
-})
+const server = http.createServer(routes)
 
 server.listen('3000', () => {
     console.log('App running');
